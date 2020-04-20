afbb210ca549d638ee2dc4a06f269973be150ae1
Lokesh Naik <pathlavathlokeshnaik@gmail.com>
2020-04-20 11:22:02 +0530

    [LOKESH NAIK]Modify.modifying few changes

--- Fundoonotes/src/main/java/com/bridgelabz/fundoonotes/controller/LabelController.java
+++ Fundoonotes/src/main/java/com/bridgelabz/fundoonotes/controller/LabelController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -46,7 +47,7 @@ public class LabelController
 
 	}
 	
-	@PostMapping("/label/delete")
+	@DeleteMapping("/label/delete")
 	public ResponseEntity<Response> deleteLabel(@RequestBody LabelUpdate label, @RequestHeader("token") String token) throws UserException, NotesException, LabelException {
 		services.deleteLabel(label, token);
 		return ResponseEntity.status(HttpStatus.OK).body(new Response("Label is deleted ", 200, label));
